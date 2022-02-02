f8e02da2f4c54a46be6ce324bda4577d7bfb6dd4
Som <somaiah1988@gmail.com>
2022-02-02 10:47:37 -0500

    02

---
+++ Java_Basics_1/src/Comments.java
@@ -0,0 +1,38 @@
+
+public class Comments {
+
+	public static void main(String[] args) {
+		
+		
+	//	the below line prints in console
+		
+		System.out.println("Hello World");
+		System.out.println("Welcome to Janbask");
+		
+		
+		//shortcut to ignore multiple lines is select the lines press ctrl+shift+/
+		//shortcut to remove multliple lines comments is press ctrl+shift+\
+		
+		
+		  System.out.println("Selenium is a automation tool");
+		  System.out.println("Latest version of selenium is 4");
+		  System.out.println("Automation is latest trend");
+		  
+		  
+		  
+		  System.out.println("Hello World"); System.out.println("Welcome to Janbask");
+		  System.out.println("Selenium is a automation tool");
+		  System.out.println("Latest version of selenium is 4");
+		 
+		System.out.println("Automation is latest trend");
+		
+		System.out.println("Hello World");
+		System.out.println("Welcome to Janbask");
+		System.out.println("Selenium is a automation tool");
+		System.out.println("Latest version of selenium is 4");
+		System.out.println("Automation is latest trend");
+
+
+	}
+
+}
---
+++ Java_Basics_1/src/printlnVsprint.java
@@ -0,0 +1,15 @@
+
+public class printlnVsprint {
+
+	public static void main(String[] args) {
+		
+		// system.out.println creates new line after
+		
+		System.out.print("Hello World");
+		System.out.println("Welcome to Janbask");
+		System.out.println("Selenium is a automation tool");
+		
+
+	}
+
+}
