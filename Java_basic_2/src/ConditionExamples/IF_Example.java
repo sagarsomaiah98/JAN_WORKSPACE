c554dd9ecb2df2d8b2cbac5df1b37db5e7ad44d7
Som <somaiah1988@gmail.com>
2022-02-03 11:22:31 -0500

    feb 3

---
+++ Java_basic_2/src/ConditionExamples/IF_Example.java
@@ -0,0 +1,17 @@
+package ConditionExamples;
+
+public class IF_Example {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		//a==30;// a is equal to 30
+		
+		
+		if(a==10)  // false
+			System.out.println("a values is 10");
+		
+
+	}
+
+}
