d1fe544ed7c669d59aab218be79f0ed7f558ee1d
Som <somaiah1988@gmail.com>
2022-02-05 06:37:12 -0500

    d

---
+++ Java_Basics_4/src/com/janbask/breakcontinueKeyword/Break_Eg.java
@@ -0,0 +1,24 @@
+package com.janbask.breakcontinueKeyword;
+
+public class Break_Eg {
+
+	public static void main(String[] args) {
+		
+		int output=9;
+		
+		
+		
+		for(int i=1;i<10;i++) 
+		
+		{
+		if(output<i)	//6<1  6<2  6<3 6<4 6<5 6<6 6<7 6<8 6<9 
+		{	
+		System.out.println(i);	
+		}	
+			
+			
+		}
+
+	}
+
+}
